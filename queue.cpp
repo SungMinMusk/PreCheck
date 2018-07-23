0838c7276a8304a7562f27b8f049fee1d680dea6
Kim Seong Min <sm3487@naver.com>
2018-07-23 16:55:36 +0900

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+/*
+	linked_list queue
+
+	node정의
+	-next;
+	-prev;
+
+	queue정의
+	-head;
+	-tail;
+
+	push() - full?
+	pop() - empty?
+  top() - empty -> return -1
+*/
+typedef struct node
+{
+	int data;
+	struct node * next;
+}node;
+
+typedef struct queue
+{
+	node*head=NULL;
+	node*tail=NULL;
+
+	void push(int a)
+	{
+		struct node* new_node = (struct node*)malloc(sizeof(struct node));
+		new_node->data = a;
+		new_node->next = NULL;
+
+		if (head == NULL && tail == NULL)
+		{
+			head = new_node;
+			tail = new_node;
+		}
+
+		tail->next = new_node;
+		tail = new_node;
+	}
+
+	void pop()
+	{
+		if (head == NULL)
+		{
+			cout << "ERROR" << endl;
+			return;
+		}
+		head = head->next;
+	}
+
+	int top()
+	{
+		if (head == NULL)
+		{
+			return -1;
+		}
+		return head->data;
+	}
+}queue;
+
+int main()
+{
+	queue q;
+	q.pop();
+	for (int i = 0; i < 10; i++)
+		q.push(i);
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << q.top() << endl;
+		q.pop();
+	}
+	
+	cout << q.top() << endl;
+	q.pop();
+	
+	return 0;
+}
